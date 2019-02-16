4617a5c7ccdb8384e8a1264e8f8dabd36f0b7dfe
yuki <>
2019-02-16 22:35:22 +0900

    test

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -153,9 +153,9 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         #    masked_image = apply_mask(masked_image, masks[1,0,0],1)
         #    masked_image = apply_mask(masked_image, mask, color)
             if class_id == 2:
-                masked_image = apply_mask(masked_image, masks[:,:,1],[0.5, 1.0, 0.0])
+                masked_image = apply_mask(masked_image, masks[:,:,1],(0.5, 1.0, 0.0))
             else:
-                masked_image = apply_mask(masked_image, masks[:,:,1],[0.5, 1.0, 0.0])
+                masked_image = apply_mask(masked_image, masks[:,:,1],(0.5, 1.0, 0.0))
         # Mask Polygon
         # Pad to ensure proper polygons for masks that touch image edges.
         padded_mask = np.zeros(
