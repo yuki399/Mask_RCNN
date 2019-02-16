64dcdad8ecdff22a10258d6e1d9ef2f8ec8a6ea5
yuki <>
2019-02-16 23:19:00 +0900

    test

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -150,8 +150,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         mask = masks[:, :, i]
         print(color)
         if show_mask:
-            pass
-            #masked_image = apply_mask(masked_image, masks[1,0,0],1)
+            masked_image = apply_mask(masked_image, masks[:,:,0],1)
         #    masked_image = apply_mask(masked_image, mask, color)
         #    if class_id == 2:
         #        masked_image = apply_mask(masked_image, masks[:,:,1],color)
